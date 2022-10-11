f09e174a782722e9fb3c06afe291af3cee68efdc
Ayrton <devayrtonsbarreto@gmail.com>
2022-10-11 09:11:57 -0300

    Deletando antigo projeto Angular e criando o projeto app-php

--- app-php/karma.conf.js
+++ app-php/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/projeto-api'),
+      dir: require('path').join(__dirname, './coverage/app-php'),
       subdir: '.',
       reporters: [
         { type: 'html' },
