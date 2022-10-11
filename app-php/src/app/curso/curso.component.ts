2d92083bcebcdeb48b07467e143a83b4a67b842e
Ayrton <devayrtonsbarreto@gmail.com>
2022-10-11 19:52:53 -0300

    Criado o componente do curso

---
+++ app-php/src/app/curso/curso.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curso',
+  templateUrl: './curso.component.html',
+  styleUrls: ['./curso.component.css']
+})
+export class CursoComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  cadastro():void{
+
+  }
+
+}
